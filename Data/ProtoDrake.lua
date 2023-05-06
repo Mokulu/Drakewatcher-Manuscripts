4d4815cdac1b542843bed583a754a5229e71d8a3
Mokulu <jsiebert9@icloud.com>
2023-05-05 22:27:53 -0700

    Added tab functionality and initial data display

--- Data/ProtoDrake.lua
+++ Data/ProtoDrake.lua
@@ -2,9 +2,28 @@ DWMS_PROTODRAKE = {
     name = 'Proto-Drake',
     ["Full Transformation"] = {
         -- Embodiment of the Storm-Eater
-        { itemId = 201790, questId = 72367, source = { note = "Raszageth, the Storm-Eater " } }
+        { itemId = 201790, questId = 72367, source = { name = "Vault of the Incarnates", note = "Raszageth, the Storm-Eater " } }
     },
     ["Skin Color"] = {
-        { itemId = 197392, questId = 69593, source = { coords = { zone = DWMS_ZONE.WAKING_SHORES, x = 25.2, y = 55.8 }, name = "Lorena Belle <Wrathion\'s Quartermaster>", note = "True Friend with Wrathion or Sabellian" }, cost = { ["i|190316"] = 1, ["c|2003"] = 400 } }
-    }
+        -- Black
+        { itemId = 197392, questId = 69593, sources = { { coords = { zone = DWMS_ZONE.WAKING_SHORES, x = 25.2, y = 55.8 }, name = "Lorena Belle <Wrathion\'s Quartermaster>", note = "True Friend with Wrathion or Sabellian", cost = { { type = "item", id = 190316, count = 1 }, { type = "currency", id = 2003, count = 400 } } } } },
+        -- Blue
+        { itemId = 197390, questId = 69591, sources = { { coords = { zone = DWMS_ZONE.AZURE_SPAN, x = 13.0, y = 49.2 }, name = "Murik", note = "Iskaara Tuskarr - Renown 19", cost = { { type = "item", id = 190329, count = 1 }, { type = "currency", id = 2003, count = 400 } } } } },
+        -- Green
+        { itemId = 197389, questId = 66720, sources = { { coords = { zone = DWMS_ZONE.OHNAHRAN_PLAINS, x = 60.4, y = 37.6 }, name = "Quartermaster Huseng", note = "Maruuk Centaur - Renown 19", cost = { { type = "item", id = 190327, count = 1 }, { type = "currency", id = 2003, count = 400 } } } } },
+        -- Bronze
+        { itemId = 197391, questId = 69592, sources = { { coords = { zone = DWMS_ZONE.VALDRAKKEN, x = 46.8, y = 78.8 }, name = "Kaestrasz", note = "Valdrakken Accord - Renown 21", cost = { { type = "item", id = 190324, count = 1 }, { type = "currency", id = 2003, count = 400 } } } } },
+    },
+    ["Skin Scale Type"] = {
+        -- Heavy
+        { itemId = 197397, questId = 69593, sources = { { coords = { zone = DWMS_ZONE.WAKING_SHORES, x = 25.9, y = 57.4 }, name = "Neltharus", drop = "Warlord Sargha" } } },
+    },
+    ["Pattern"] = {
+        -- Predator
+        { itemId = 197394, questId = 69595, sources = { { name = "Inscription" } } },
+        -- Harrier
+        { itemId = 197395, questId = 69596, sources = { { coords = { zone = DWMS_ZONE.WAKING_SHORES, x = 25.2, y = 55.8 }, name = "Cataloger Jakes", note = "Dragonscale Expedition - Renown 15", cost = { { type = "currency", id = 2003, count = 100 } } } } },
+        -- Skyterror
+        { itemId = 197396, questId = 69597, sources = { { coords = { zone = DWMS_ZONE.AZURE_SPAN, x = 11.5, y = 49.1 }, name = "Brackenhide Hallow", drop = "Decay Tainted Chest" } } },
+    },
 }
--- DrakewatcherManuscripts.lua
+++ DrakewatcherManuscripts.lua
@@ -1,4 +1,5 @@
 DrakewatcherManuscripts = LibStub("AceAddon-3.0"):NewAddon("DrakewatcherManuscripts", "AceEvent-3.0")
+local AceGUI = LibStub("AceGUI-3.0")
 
 -- WoW Apis
 local CreateFrame, GetScreenWidth, GetScreenHeight = CreateFrame, GetScreenWidth, GetScreenHeight
@@ -46,6 +47,7 @@ function DrakewatcherManuscripts:CreateFrame()
     f:SetPortraitToAsset(GetAddonMetadata("DrakewatcherManuscripts", "IconTexture"))
     -- get addon info
     f.window = "default"
+    self.frame = f
 
     -- position the frame
     local xOffset, yOffset = db.frame.xOffset, db.frame.yOffset
@@ -84,25 +86,122 @@ function DrakewatcherManuscripts:CreateFrame()
         saveWindowDrag()
     end)
 
+    -- progress bar
+    local progressBar = CreateFrame("Frame", nil, f, "TooltipBackdropTemplate")
+    progressBar:SetBackdropBorderColor(0.5,0.5,0.5)
+    progressBar:SetSize(120,20)
+    progressBar:SetPoint("CENTER", f, "TOP", 0, -42)
+    progressBar.bar = CreateFrame("StatusBar", nil, progressBar)
+    progressBar.bar:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")
+    progressBar.bar:SetStatusBarColor(0.1, 1, 0.1)
+    progressBar.bar:SetPoint("TOPLEFT", 5, -5)
+    progressBar.bar:SetPoint("BOTTOMRIGHT", -5, 5)
+    progressBar.text = progressBar.bar:CreateFontString()
+    progressBar.text:SetFontObject(GameFontNormal)
+    progressBar.text:SetTextColor(1,1,1)
+    progressBar.text:SetTextScale(0.8)
+    progressBar.text:SetPoint("CENTER")
+    progressBar.text:SetJustifyH("CENTER")
+    progressBar.text:SetJustifyV("CENTER")
+
+    f.progressBar = progressBar
+
+    -- copying mixins to statusbar
+    Mixin(progressBar.bar,SmoothStatusBarMixin)
+
+    -- using mixin methods
+    local owned, total = countManuscripts()
+    progressBar.bar:SetMinMaxSmoothedValue(0, total)
+    progressBar.bar:SetSmoothedValue(owned)
+    progressBar.text:SetText(owned.."/"..total)
+
     local tabs = {}
+    _self = self
     -- tabs
     for i, v in ipairs(DWMS_DRAKE_DATA) do
         local button = CreateFrame("Button", "$parentTab" .. i, f, "PanelTopTabButtonTemplate", i)
         if i > 1 then
             button:SetPoint("LEFT", "$parentTab" .. (i-1), "RIGHT", -16, 0);
         else
-            button:SetPoint("TOPLEFT", f, "TOPLEFT", 58, -28)
+            button:SetPoint("TOPLEFT", f, "TOPLEFT", 6, -50)
         end
         button:SetText(v.name)
         button:SetScript("OnClick", function()
-            PanelTemplates_SetTab(f, i)
+            _self:SelectTab(i)
         end)
         tabs[i] = button
         PanelTemplates_SetNumTabs(f, i);
     end
-    PanelTemplates_SetTab(f, 1)
+    f.tabs = tabs
 
-    self.frame = f
+    -- content windows
+    local contentWindows = {}
+    for i, drake in ipairs(DWMS_DRAKE_DATA) do
+        local contentWindow = AceGUI:Create("InlineGroup")
+        contentWindows[i] = contentWindow
+        contentWindow:SetLayout("Fill")
+        contentWindow.frame:SetParent(f)
+        contentWindow.frame:SetPoint("TOPLEFT", f, "TOPLEFT", 4, -62)
+        contentWindow.frame:SetPoint("BOTTOMRIGHT", f, "BOTTOMRIGHT", -4, 4)
+
+        local frame = AceGUI:Create("ScrollFrame")
+        frame:SetLayout("Flow")
+        frame:SetFullWidth(true)
+        contentWindow:AddChild(frame)
+        contentWindow.scrollFrame = frame
+
+        for catName, cat in pairs(drake) do
+            if catName ~= "name" then
+                local label = AceGUI:Create("Label")
+                label:SetText(catName)
+                label:SetFontObject(GameFontNormalLarge)
+                label:SetColor(0.4, 0.73, 1)
+                label:SetFullWidth(true)
+                frame:AddChild(label)
+
+                -- loop 10 times
+                for _, manuscript in ipairs(cat) do
+                    local item = Item:CreateFromItemID(manuscript.itemId)
+                    local isObtained = C_QuestLog.IsQuestFlaggedCompleted(manuscript.questId)
+                    local itemFrame = AceGUI:Create("InteractiveLabel")
+                    frame:AddChild(itemFrame)
+
+                    item:ContinueOnItemLoad(function()
+                        local name = item:GetItemName()
+                        -- split name by :
+                        local nameProps = { strsplit(":", name) }
+                        local icon = item:GetItemIcon()
+                        local quality = item:GetItemQuality()
+                        local r, g, b = GetItemQualityColor(quality)
+
+                        itemFrame:SetImage(icon)
+                        itemFrame:SetImageSize(32, 32)
+                        itemFrame:SetText(nameProps[2] or name)
+                        itemFrame:SetDisabled(not isObtained)
+                        --itemFrame:SetHighlight(r, g, b, 1)
+                    end)
+                end
+            end
+        end
+    end
+
+    f.contentWindows = contentWindows
+    self:SelectTab(1)
+
+    -- insert into special frames table
+    _G["DrakewatcherManuscriptsFrame"] = f
+    tinsert(UISpecialFrames, "DrakewatcherManuscriptsFrame")
+end
+
+function DrakewatcherManuscripts:SelectTab(tabIndex)
+    PanelTemplates_SetTab(self.frame, tabIndex)
+    for i, v in ipairs(self.frame.contentWindows) do
+        if i == tabIndex then
+            v.frame:Show()
+        else
+            v.frame:Hide()
+        end
+    end
 end
 
 function DrakewatcherManuscripts:ToggleFrame()
@@ -126,6 +225,20 @@ function DrakewatcherManuscripts:PLAYER_ENTERING_WORLD()
     self:ToggleFrame()
 end
 
+function countManuscripts()
+    local owned, total = 0, 0
+    for _, drake in ipairs(DWMS_DRAKE_DATA) do
+        for k, cats in pairs(drake) do
+            if k ~= "name" then
+                for _, manuscript in ipairs(cats) do
+                    owned = owned + (C_QuestLog.IsQuestFlaggedCompleted(manuscript.questId) and 1 or 0)
+                    total = total + 1
+                end
+            end
+        end
+    end
+    return owned, total
+end
 
 function DwMs_OnAddonCompartmentClick()
     DrakewatcherManuscripts:ToggleFrame()
